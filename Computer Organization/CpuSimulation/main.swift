32e4fae797454fb054c24c2e880ee33a74179b52
Stas <stasel@gmail.com>
2015-11-29 23:05:32 +0200

    new project - cpu simulator

---
+++ Computer Organization/CpuSimulation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CpuSimulation
+//
+//  Created by Stas Seldin on 29/11/2015.
+//  Copyright © 2015 Stas Seldin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Computer Organization/README.md
@@ -0,0 +1,6 @@
+20272 Digital systems
+http://www.openu.ac.il/courses/20272.htm
+
+
+20471 Computer organization
+http://www.openu.ac.il/courses/20471.htm
