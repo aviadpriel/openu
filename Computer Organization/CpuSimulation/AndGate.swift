8745416d51ff66f24af2db017254ccbd716684c1
Stas <stasel@gmail.com>
2016-01-17 22:48:19 +0200

    delete cpu project

--- Computer Organization/CpuSimulation/AndGate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AndGate.swift
-//  CpuSimulation
-//
-//  Created by Stas Seldin on 01/12/2015.
-//  Copyright © 2015 Stas Seldin. All rights reserved.
-//
-
-import Foundation
--- Computer Organization/CpuSimulation/Components.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Components.swift
-//  CpuSimulation
-//
-//  Created by Stas Seldin on 30/11/2015.
-//  Copyright © 2015 Stas Seldin. All rights reserved.
-//
-
-import Foundation
