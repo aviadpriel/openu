38c394f0b008491366dd99bf70a49f8843eb3bb5
Stas <stasel@gmail.com>
2016-01-30 22:57:40 +0200

    Added weight for edges

--- DataStructures/DataStructures/Graph/Edge.swift
+++ DataStructures/DataStructures/Graph/Edge.swift
@@ -11,6 +11,7 @@ import Foundation
 class Edge<T>: NSObject {
     let source: Vertex<T>
     let destination: Vertex<T>
+    let weight: Double = 0.0
     
     init(source: Vertex<T>, destination: Vertex<T>) {
         self.source = source
